dc5a911af5e27c1baf0204d9604caa54998fa394
Attilio Chiavegatti <attiliochiavegati@gmail.com>
2022-03-25 08:39:34 -0300

    gen3d update

--- python_utils/gen3d.py
+++ python_utils/gen3d.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import argparse
 from rdkit import Chem
@@ -8,8 +10,8 @@ sys.stdout.reconfigure(line_buffering=True)
 # arguments parsing #
 #####################
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file')
-
+parser.add_argument('file', help='2 column CSV file. 1st column is the molecule id, 2nd column is the SMILES string')
+parser.add_argument('--prefix', '--p', help='prefix for the resulting filenames', default='mol')
 args = parser.parse_args()
 
 def generate_3D_coords(smiles_string):
@@ -19,7 +21,7 @@ def generate_3D_coords(smiles_string):
     return molecule_with_H
 
 with open(args.file) as file:
-    file.readline() # ignore header in the first line
+    #file.readline() # ignore header in the first line
     dataset = file.readlines()
 
 for entry in dataset:
@@ -39,9 +41,9 @@ for entry in dataset:
         print(f"DOT string: {dot_string}")
 
         dot_molecule = generate_3D_coords(dot_string)
-        Chem.MolToXYZFile(dot_molecule, f"cat_{id}_dot.xyz")
+        Chem.MolToXYZFile(dot_molecule, f"{args.prefix}_{id}_dot.xyz")
 
     molecule = generate_3D_coords(smiles_string)
-    Chem.MolToXYZFile(molecule, f"cat_{id}.xyz")
+    Chem.MolToXYZFile(molecule, f"{args.prefix}_{id}.xyz")
 
     print(f"End of processing for molecule ID {id}\n")
