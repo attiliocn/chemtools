4824b720121914d5d6a53a11a0472ddd3d8cf0e6
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-02-24 11:43:44 -0300

    feat: add extractor for g16 calcss

---
+++ tools/extract_g16.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+if [[ -d results_extracted ]]; then
+    echo "The folder results_extracted already exists."
+    echo "Delete this folder and try again"
+    exit
+else
+    mkdir results_extracted 
+    find . -mindepth 2 -type f \( -name "*.com" -o -name "*.log" \) | xargs -I{} cp {} results_extracted
+fi
+
+
+
+
