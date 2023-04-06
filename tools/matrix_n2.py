19658c881dabeb618f9e3e7ad1b3a5540cdefadf
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-04-06 10:08:32 -0300

    fix: bug in matrix numbering

--- tools/matrix_n2.py
+++ tools/matrix_n2.py
@@ -17,28 +17,38 @@ for element in args.swap:
 src_numbering = all_numbers[0::2]
 dest_numbering = all_numbers[1::2]
 
+
 def reorder_xyz(xyz_matrix, src_numbering, dest_numbering):
     n_atoms = len(xyz_matrix)
     modified_matrix = np.zeros(n_atoms*4).reshape(n_atoms,4)
     modified_matrix = modified_matrix.astype(xyz_matrix.dtype)
-
-    empty_in_destination = []
-    for i in range(n_atoms):
-        if i not in dest_numbering:
-            empty_in_destination.append(i)
-    empty_in_destination = np.array(empty_in_destination)
 
     unmodified_in_source = []
     for i in range(n_atoms):
         if i not in src_numbering:
             unmodified_in_source.append(i)
     unmodified_in_source = np.array(unmodified_in_source)
+    print(f"SRC: {np.array(src_numbering)+1}")
+    print(unmodified_in_source+1)
+    print(len(unmodified_in_source))
 
-    for src, dest in zip(src_numbering, dest_numbering):
-        modified_matrix[dest] = xyz_matrix[src]
+    empty_in_destination = []
+    for i in range(n_atoms):
+        if i not in dest_numbering:
+            empty_in_destination.append(i)
+    empty_in_destination = np.array(empty_in_destination)
+    
+    print(f"DEST: {np.array(dest_numbering)+1}")
+    print(empty_in_destination+1)
+    print(len(empty_in_destination))
 
     for src, dest in zip(empty_in_destination, unmodified_in_source):
-        modified_matrix[dest] = xyz_matrix[src]
+        print(f"S:{src+1} --> D:{dest+1}")
+        modified_matrix[src] = xyz_matrix[dest]
+
+    for src, dest in zip(src_numbering, dest_numbering):
+       print(f"S:{src+1} --> D:{dest+1}")
+       modified_matrix[dest] = xyz_matrix[src]
     
     return modified_matrix
 
