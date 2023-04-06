6d2e0d8fe91f6b04237548535f8bfd22a3ef0272
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-04-06 10:28:30 -0300

    feat: add verbose option for debug

--- tools/matrix_n2.py
+++ tools/matrix_n2.py
@@ -6,6 +6,7 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('-f','--files',nargs='+',required=True)
 parser.add_argument('-s','--swap',nargs='+',type=int,action='append',required=True)
+parser.add_argument('--verbose', action='store_true', help='Print log information')
 args = parser.parse_args()
 
 all_numbers = []
@@ -28,27 +29,32 @@ def reorder_xyz(xyz_matrix, src_numbering, dest_numbering):
         if i not in src_numbering:
             unmodified_in_source.append(i)
     unmodified_in_source = np.array(unmodified_in_source)
-    print(f"SRC: {np.array(src_numbering)+1}")
-    print(unmodified_in_source+1)
-    print(len(unmodified_in_source))
+    
 
     empty_in_destination = []
     for i in range(n_atoms):
         if i not in dest_numbering:
             empty_in_destination.append(i)
     empty_in_destination = np.array(empty_in_destination)
-    
-    print(f"DEST: {np.array(dest_numbering)+1}")
-    print(empty_in_destination+1)
-    print(len(empty_in_destination))
 
-    for src, dest in zip(empty_in_destination, unmodified_in_source):
-        print(f"S:{src+1} --> D:{dest+1}")
-        modified_matrix[src] = xyz_matrix[dest]
+    if args.verbose:
+        print(f"SRC: {np.array(src_numbering)+1}")
+        print(unmodified_in_source+1)
+        print(len(unmodified_in_source))
+
+        print(f"DEST: {np.array(dest_numbering)+1}")
+        print(empty_in_destination+1)
+        print(len(empty_in_destination))
+
+    for src, dest in zip(unmodified_in_source, empty_in_destination):
+        if args.verbose:
+            print(f"S:{src+1} --> D:{dest+1}")
+        modified_matrix[dest] = xyz_matrix[src]
 
     for src, dest in zip(src_numbering, dest_numbering):
-       print(f"S:{src+1} --> D:{dest+1}")
-       modified_matrix[dest] = xyz_matrix[src]
+        if args.verbose:    
+            print(f"S:{src+1} --> D:{dest+1}")
+        modified_matrix[dest] = xyz_matrix[src]
     
     return modified_matrix
 
@@ -66,7 +72,7 @@ def reorder_matrix_rows(files):
             f.write("\n".join(xyz_header))
             f.write("\n")
             for i in xyz_matrix:
-                f.write("{: >3} {: >20} {: >20} {: >20}\n".format(*i))
+                f.write("{: <2}{: >20}{: >20}{: >20}\n".format(*i))
 
 if __name__ == '__main__':
     reorder_matrix_rows(args.files)
