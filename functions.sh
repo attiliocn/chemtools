c27b9785000e832289679d0ac6ba8c03a27416ac
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-06-02 08:45:55 -0300

    fix: refactor of gvv function

--- functions.sh
+++ functions.sh
@@ -1,10 +1,18 @@
 function gvv(){
+    if [[ $1 == 'conv' ]]; then
+        force_conversion=true
+        shift
+    else
+        force_conversion=false
+    fi
+
     for file in $@; do
         file_extension="${file#*.}"
-        if [[ $file_extension == 'xyz' ]]; then
-            obabel -ixyz $file -omol > /tmp/"${file/.xyz/.mol}"
-            gv "/tmp/${file/.xyz/.mol}"
-            rm "/tmp/${file/.xyz/.mol}"
+        converted_filename="/tmp/${file/"$file_extension"/.mol}"
+        if [[ $file_extension == 'xyz' || $force_conversion ]]; then
+            obabel -ixyz $file -omol > $converted_filename
+            gv $converted_filename
+            rm $converted_filename
         else
           gv $file
         fi
