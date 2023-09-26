c19a8288f60f6eefdfbdaadeed1d602619f72ae0
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-09-26 07:17:44 -0300

    feat: open orca output files using GVV

--- functions/gvv.sh
+++ functions/gvv.sh
@@ -21,8 +21,17 @@ function gvv(){
         filename=$(basename "$filepath")
         file_extension=$(echo "$filename" | awk -F . '{print $NF}')
         converted_filename=""$temp_directory"/${filename/"$file_extension"/mol}"
+        
         if [[ $file_extension == 'xyz' || $force_conversion == true ]]; then
             obabel -ixyz $filepath -omol > $converted_filename
+        # elif [[ $file_extension == 'out' && $force_conversion == false ]]; then
+        #     echo "ORCA output. The optimized geometry will be extracted manually"
+        #     outxyz.sh $filepath
+        #     obabel -ixyz "${filepath/."$file_extension"/.xyz}" -omol > $converted_filename
+        elif [[ $file_extension == 'out' ]]; then
+            echo "ORCA output. The output will be converted using OFakeG"
+            OfakeG "$filepath"
+            mv ${filepath/."$file_extension"/_fake."$file_extension"} $temp_directory/${filename/"$file_extension"/log}
         else
             cp $filepath "$temp_directory"
         fi
