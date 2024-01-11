fd7ed27110b3a50dc2122c27a453a7185be51c3e
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-01-11 09:37:43 -0300

    fix orca output extractor

--- tools/extract_orca.sh
+++ tools/extract_orca.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if [[ -d results_extracted ]]; then
+if [[ ! -d results_extracted ]]; then
     mkdir results_extracted
     find . -mindepth 2 -type f \( -name "*.out" -o -name "*.inp" -o -name "*.hess" \) -a -not \( -name "*smd*" -o -name "*scf*" -o -name "*atom*" \) -exec cp -v {} results_extracted/ \;
 else
     exit
-
