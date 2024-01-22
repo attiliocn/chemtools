7c74072fbed544807239af14abadc17c05c7e845
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-01-22 20:35:43 -0300

    convert DOS to UNIX inplace when converting gjf to xyz

--- converters/gjfxyz.sh
+++ converters/gjfxyz.sh
@@ -3,16 +3,21 @@
 for i in $@; do
 	echo "GJF ---> XYZ $i"
 
+    # convert DOS carriage return to UNIX CR
 	tr -d '\15\32' < $i > temp
 	mv temp $i
-	coordinatesLine1=$(grep -n "^$" $i | head -n 2 | tail -1 | sed 's/://')
-	coordinatesLine1=$((coordinatesLine1 + 2))
-	#coordinatesLine1=$(grep -nE "^\s+" $i | head -n 1 | sed 's/:/\t/'| awk '{print $1}')
-	tail -n +"$coordinatesLine1" $i | sed '/^$/d'| sed '${/^ $/d}' > temp
+
+    # remove Gaussian Conectivity Table
+    head -n $(grep -n "^$" "$i" | head -n 3 | tail -1 | sed 's/://') "$i" > temp
+	mv temp $i
+
+	coordinates_first_line=$(grep -n "^$" $i | head -n 2 | tail -1 | sed 's/://')
+	coordinates_first_line=$((coordinates_first_line + 2))
+	tail -n +"$coordinates_first_line" $i | sed '/^$/d'| sed '${/^ $/d}' > temp
 	cat temp | wc -l > ${i%.*}.xyz
 	echo ${i%.*} >> ${i%.*}.xyz
 	cat temp >> ${i%.*}.xyz
+    rm temp
 	echo "-----------"
 done
-rm temp
 
--- tools/remove_gaussian_connectivity.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-files=$@
-
-for i in $files; do 
-	head -n $(grep -n "^$" "$i" | head -n 3 | tail -1 | sed 's/://') "$i" > temp
-	mv temp $i
-done
