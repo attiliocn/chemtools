5e07159c3947b915e663a3a33339ebf113191bc5
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-01-23 13:12:16 -0300

    update orca extractor

--- tools/extract_orca.sh
+++ tools/extract_orca.sh
@@ -2,7 +2,23 @@
 
 if [[ ! -d results_extracted ]]; then
     mkdir results_extracted
-    find . -mindepth 2 -type f \( -name "*.out" -o -name "*.inp" -o -name "*.hess" \) -a -not \( -name "*smd*" -o -name "*scf*" -o -name "*atom*" \) -exec cp -v {} results_extracted/ \;
+
+    find . -mindepth 2 -type f \
+        \( \
+            -name "*.out" \
+            -o -name "*.inp" \
+            -o -name "*.hess" \
+            -o -name "*_trj.xyz" \
+        \) \
+        -a -not \
+        \( \
+            -name "*smd*" \
+            -o -name "*scf*" \
+            -o -name "*atom*" \
+            -o -name "*proc*" \
+            -o -name "*appr*" \
+        \) \
+        -exec cp -v {} results_extracted/ \;
 else
     echo "Error: The results_extracted directory already exists"
 fi
