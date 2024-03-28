afd916a28f532dcbd2be710a0b6228c1740acec1
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-03-28 15:24:03 -0300

    update gvv to work outside cwd

--- functions/gvv.sh
+++ functions/gvv.sh
@@ -17,18 +17,23 @@ function gvv(){
     fi
 
     for file in $@; do
-        file_extension=$(echo "$file" | awk -F . '{print $NF}')
-        file_basename=${file/."$file_extension"/}
+        filename=$(basename $file)
+        file_extension=$(echo "$filename" | awk -F . '{print $NF}')
+        file_basename=${filename/."$file_extension"/}
         
         if [[ $file_extension == 'xyz' || $force_conversion == true ]]; then
             obabel -ixyz $file -omol > "$temp_directory"/"$file_basename".mol
+        
         elif [[ $file_extension == 'out' ]]; then
             echo "ORCA output. The output will be converted using OFakeG"
-            OfakeG $file &> /dev/null
+            cp $file $temp_directory
+            OfakeG "$temp_directory"/"$file" &> /dev/null
+            rm "$temp_directory"/"$file"
             mv "$file_basename"_fake.out "$temp_directory"/"$file_basename".log
+        
         else
             cp $file "$temp_directory"
         fi
     done
-    gv "$temp_directory"/* &> /dev/null &
+    gv "$temp_directory"/* & &> /dev/null
   }
