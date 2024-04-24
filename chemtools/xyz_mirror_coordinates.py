0e073229b0ff67631a669cd2fb07c1e5db258265
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-04-24 19:10:29 -0300

    add header to mirror xyz script

--- chemtools/xyz_mirror_coordinates.py
+++ chemtools/xyz_mirror_coordinates.py
@@ -2,7 +2,7 @@
 
 from argparse import ArgumentParser
 import numpy as np
-from util.xyzutils import read_xyz_file, write_xyz_file
+from util.xyzutils import read_xyz_file, build_xyz_file
 
 argparser = ArgumentParser()
 argparser.add_argument('files', help='xyz file to mirror', nargs='+')
@@ -15,4 +15,6 @@ for file in args.files:
     basename = file.split("/")[-1].split(".")[0]
     elements, coords = read_xyz_file(file)
     coords_mirror = mirror_xyz(coords)
-    write_xyz_file(elements, coords_mirror, f"{basename}_mirror.xyz")
+    _ = build_xyz_file(elements, coords_mirror, header=f"{basename}_mirror.xyz")
+    with open(f"{basename}_mirror.xyz", mode='w') as f:
+        f.write(_)
