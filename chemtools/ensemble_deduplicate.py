c90a2abab0a489cf92f80c51331739ea4bcf5d6c
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-20 11:11:55 -0300

    implement new ensemble deduplication tool

---
+++ chemtools/ensemble_deduplicate.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import argparse
+
+from modules.xyzutils import read_xyz_ensemble, build_xyz_file
+from modules.geometry import rmsd_matrix, get_duplicates_rmsd_matrix
+
+parser = argparse.ArgumentParser()
+parser.add_argument('files', nargs='+', help='XYZ Ensemble Files')
+parser.add_argument('--threshold', type=float, default=.25, help='RMSD threshold for duplicate detection. Default is 0.25')
+args = parser.parse_args()
+
+for file in args.files:
+    basename, extension = file.split('.', 1)
+    basename_updated = f"{basename}_deduplicated.{extension}"
+
+    ensemble = read_xyz_ensemble(file)
+    numconfs = len(ensemble)
+    coordinates_all = [_['coordinates'] for _ in ensemble.values()]
+    elements_all = [_['elements'] for _ in ensemble.values()]
+    header_all = [_['header'] for _ in ensemble.values()]
+    
+    rmsd_distance_matrix = rmsd_matrix(coordinates_all)
+    to_delete = get_duplicates_rmsd_matrix(rmsd_distance_matrix, threshold=args.threshold)
+
+    _coordinates = [coordinates_all[i] for i in range(numconfs) if i not in to_delete]
+    _elements = [elements_all[i] for i in range(numconfs) if i not in to_delete]
+    _header = [header_all[i] for i in range(numconfs) if i not in to_delete]
+
+    with open('deduplicate.log', mode='w') as f:
+        f.write(f"Current file: {basename}\n")
+        f.write(f"Number of conformers: {numconfs}\n")
+        f.write(f"Conformers after deduplication: {len(_coordinates)}\n")
+        f.write(f"{numconfs - len(_coordinates)} conformers were deleted\n")
+        f.write(f"{len(_coordinates)} conformers will be written to {basename_updated}\n\n")
+
+    with open(basename_updated, mode='w') as f:
+        for header, elements, coordinates in zip(_header, _elements, _coordinates):  
+            _ = build_xyz_file(
+                elements, 
+                coordinates,
+                header=header
+            )
+            f.write(_)
+        
