f921b4dabf6302b5ec309754471d18ac621a290f
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-21 15:49:17 -0300

    bugfix for ensemble comparison engine

--- chemtools/ensemble_compare.py
+++ chemtools/ensemble_compare.py
@@ -15,7 +15,6 @@ from modules.rdkitutils import convert_coordinates_to_mols
 parser = argparse.ArgumentParser()
 parser.add_argument('ensemble1', help='XYZ Ensemble no 1')
 parser.add_argument('ensemble2', help='XYZ Ensemble no 2')
-parser.add_argument('reference', help='Reference XYZ Structure')
 args = parser.parse_args()
 
 log = open('rmsd-compare.log', mode='w', buffering=1)
@@ -40,37 +39,36 @@ log.write(f"Ensemble A has {len(mols_a)} molecules\n")
 log.write(f"Ensemble B has {len(mols_b)} molecules\n")
 log.write(f"A ({len(mols_a)},{len(mols_b)}) matrix of RMSD will be calculated\n")
 
-ref_elements, ref_coordinates = read_xyz_file(args.reference)
-ref_mol = convert_coordinates_to_mols(ref_elements, ref_coordinates)
-
 all_mols = np.concatenate([mols_a, mols_b])
 
 log.write('Determining MCS (Maximum Common Substructure)\n')
 
-params = rdFMCS.MCSParameters()
-params.AtomTyper = rdFMCS.AtomCompare.CompareElements
-params.BondCompareParameters.RingMatchesRingOnly = True
-params.BondCompareParameters.CompleteRingsOnly = True
 mcs = rdFMCS.FindMCS(all_mols)
 pattern = Chem.MolFromSmarts(mcs.smartsString)
 
-log.write('MCS was determined\n')
+log.write('MCS was determined using all atoms\n')
 log.write(f"MCS has {mcs.numAtoms} atoms\n")
 log.write(f"MCS SMARTS Pattern is {mcs.smartsString}\n\n")
+log.write('Removing hydrogens from the SMARTS patterns\n')
+pattern = Chem.RemoveAllHs(pattern)
+mcs_heavy = Chem.MolToSmarts(pattern)
+log.write(f'MCS SMARTS Pattern (heavy-atoms) is {mcs_heavy}\n\n')
 
-log.write('Calculating RMSD Matrix (it may take a while)\n')
+log.write('Calculating heavy-atom RMSD Matrix (it may take a while)\n')
 
 rmsd_matrix = np.zeros((len(mols_a), len(mols_b)))
 for i in range(rmsd_matrix.shape[0]):
-    ref_mol = mols_a[i]
-    ref_match = ref_mol.GetSubstructMatch(pattern)
+    mol_ref = mols_a[i]
+    ref_match = mol_ref.GetSubstructMatch(pattern)
     for j in range(rmsd_matrix.shape[1]):
         probe_mol = mols_b[j]
         mv = probe_mol.GetSubstructMatch(pattern)
-        rmsd, R = rdMolAlign.GetAlignmentTransform(probe_mol, ref_mol, atomMap=list(zip(mv,ref_match)))
+        rmsd = rdMolAlign.AlignMol(probe_mol, mol_ref, atomMap=list(zip(mv,ref_match)))
         rmsd_matrix[i, j] = rmsd
-print(R)
-print(R.shape)
+
+with open(f"{args.ensemble2.rsplit('.')[0]}_aligned.xyz", mode='w') as f:
+    for mol in mols_b:
+        f.write(Chem.MolToXYZBlock(mol))
 
 log.write('Finished\n')
 log.write('\n\n')
@@ -83,6 +81,8 @@ fig, ax = plt.subplots(dpi=300)
 heatmap = sns.heatmap(
     data=rmsd_matrix
 )
+heatmap.set_xlabel(f"{args.ensemble2}")
+heatmap.set_ylabel(f"{args.ensemble1}")
 fig.tight_layout()
 plt.savefig('rmsd-compare-rmsd.png')
 
