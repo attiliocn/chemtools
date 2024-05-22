2030e5096068618f44b8abcc4c2c2116677fd2e0
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-22 11:04:35 -0300

    fixed installation script

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-for f in "$CHEMTOOLS_DIR"/functions/*.sh; do
+for f in "$CHEMTOOLS_DIR"/chemtools/functions/*.sh; do
     source $f
 done
 
-for f in "$CHEMTOOLS_DIR"/run-softwares/*.sh; do
+for f in "$CHEMTOOLS_DIR"/chemtools/run-softwares/*.sh; do
     source $f
 done
 
-export PATH=""$PATH":"$CHEMTOOLS_DIR"/tools"
-export PATH=""$PATH":"$CHEMTOOLS_DIR"/converters"
-export PATH=""$PATH":"$CHEMTOOLS_DIR"/shell"
+export PATH=""$PATH":"$CHEMTOOLS_DIR"/chemtools"
+export PATH=""$PATH":"$CHEMTOOLS_DIR"/chemtools/tools"
+export PATH=""$PATH":"$CHEMTOOLS_DIR"/chemtools/converters"
+export PATH=""$PATH":"$CHEMTOOLS_DIR"/chemtools/shell"
 
