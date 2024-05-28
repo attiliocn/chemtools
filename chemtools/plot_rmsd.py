37886d9e4a60a42053d28786317bcc3d0efa892f
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-28 17:17:00 -0300

    add option to change the linkage method fot clustermap plot

--- chemtools/plot_rmsd.py
+++ chemtools/plot_rmsd.py
@@ -7,6 +7,7 @@ import pandas as pd
 parser = argparse.ArgumentParser()
 parser.add_argument('-f','--matrix', help='RMSD matrix in csv format')
 parser.add_argument('--triangular', action='store_true', help='The RMSD matrix is in the (lower) triangular form')
+parser.add_argument('--linkage', default='average', help='Linkage Method for Hierarchical Clustering. See <https://seaborn.pydata.org/generated/seaborn.clustermap.html> for details')
 args = parser.parse_args()
 
 rmsd_matrix = pd.read_csv(args.matrix, header=None, index_col=None)
@@ -27,7 +28,8 @@ plt.savefig('plot_rmsd.png')
 plt.close('all')
 fig, ax = plt.subplots(dpi=300)
 cluster = sns.clustermap(
-    data=rmsd_matrix
+    data=rmsd_matrix,
+    method=args.linkage
 )
 fig.tight_layout()
 plt.savefig('plot_rmsd-cluster.png')
