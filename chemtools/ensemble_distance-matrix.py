c1bb1fd10d62dd046414217cc94f95ef0ece191f
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-05-29 10:05:47 -0300

    remove -f option

--- chemtools/ensemble_distance-matrix.py
+++ chemtools/ensemble_distance-matrix.py
@@ -5,10 +5,10 @@ from modules import geometry, xyzutils
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file', help='XYZ Ensemble File')
+parser.add_argument('ensemble', help='XYZ Ensemble File')
 args = parser.parse_args()
 
-ensemble = xyzutils.read_xyz_ensemble(args.file)
+ensemble = xyzutils.read_xyz_ensemble(args.ensemble)
 coordinates_all = [_['coordinates'] for _ in ensemble.values()]
 
 rmsd_distance_matrix = geometry.rmsd_matrix_parallel(coordinates_all)
