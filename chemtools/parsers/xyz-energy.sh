f59f7c5e32f9f18a8480cb373f7ba476a791e468
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-06-03 15:41:19 -0300

    fix xyz-energy script to use regex for energies

--- chemtools/parsers/xyz-energy.sh
+++ chemtools/parsers/xyz-energy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "file,header" > headers.csv
+echo "file,header" > energy.csv
 for f in $@
 do
-    headers=$(grep -P "^-[0-9]+\.[0-9]" $f)
+    energies=$(head -n 2 $f | tail -1 | grep -oP "\-[0-9]+\.[0-9]+")
     i=1
-    for h in $headers
+    for h in $energies
     do
-        echo "${f/.*/}"_"$i".xyz,$h >> headers.csv
+        echo "${f/.*/}"_"$i".xyz,$h >> energy.csv
         i=$((i+1))
     done
 done
