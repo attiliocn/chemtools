31cce36a97618d86ad26fac03b73dd8357e5a856
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-07-01 15:10:55 -0300

    add only light atoms as default for distance matrix calculation

--- chemtools/ensemble_distance-matrix.py
+++ chemtools/ensemble_distance-matrix.py
@@ -6,10 +6,20 @@ import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('ensemble', help='XYZ Ensemble File')
+parser.add_argument('--all-atoms', action='store_true', help='Include H atoms in the RMSD calculation. Default is use only heavy atoms')
 args = parser.parse_args()
+
+basename, extension = args.ensemble.rsplit('.', 1)
 
 ensemble = xyzutils.read_xyz_ensemble(args.ensemble)
 coordinates_all = [_['coordinates'] for _ in ensemble.values()]
+elements_all = [_['elements'] for _ in ensemble.values()]
 
-rmsd_distance_matrix = geometry.rmsd_matrix_parallel(coordinates_all)
-np.savetxt("rmsd.csv", rmsd_distance_matrix, delimiter=",")
+if args.all_atoms:
+        coordinates_rmsd = coordinates_all
+else:
+    heavy_atoms = np.where(elements_all[0] != 'H')[0]
+    coordinates_rmsd = [i[heavy_atoms,:] for i in coordinates_all]
+
+rmsd_distance_matrix = geometry.rmsd_matrix_parallel(coordinates_rmsd)
+np.savetxt(f"{basename}.csv", rmsd_distance_matrix, delimiter=",")
