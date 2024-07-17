de4a105474bff85236fb6d464b6bbe038fbe22ed
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-07-17 14:02:01 -0300

    remove rmsd calculator from ensemble_deduplicate

--- chemtools/ensemble_deduplicate.py
+++ chemtools/ensemble_deduplicate.py
@@ -1,62 +1,41 @@
 #!/usr/bin/env python3
 
-# TODO: make this script operate only in .csv lower triangular distance matrix
-# thus avoiding the need to refactor several rmsd engines every time a bug
-# comes out
-
 # TODO: use __main__ to loop through inputs, instead of a fucking 
 # _for_ loop
 
 import argparse
 import time
-import os
 
 import numpy as np
-
-from modules import geometry, rdkitutils, xyzutils
+from modules import geometry, xyzutils
 
 start_time_global = time.time()
 
 parser = argparse.ArgumentParser()
-parser.add_argument('files', nargs='+', help='XYZ Ensemble Files')
+parser.add_argument('files', nargs='+', help='.csv distance matrix in lower triangular format')
 parser.add_argument('--threshold', type=float, default=.25, help='RMSD threshold for duplicate detection. Default is 0.25')
-parser.add_argument('--max-matches', '-c', type=int, default=1000, help='Maximum number of symmetric substructures to consider. Default is 1000')
 args = parser.parse_args()
 
 log = open('deduplicate.log', mode='w', buffering=1)
 log.write(f"THRESHOLD: {args.threshold}\n")
 
 for file in args.files:
+    start_time = time.time()
+
     basename, extension = file.rsplit('.', 1)
     basename_updated = f"{basename}_deduplicated.{extension}"
+    log.write(f"Current file: {basename}\n")
 
-    ensemble = xyzutils.read_xyz_ensemble(file)
-    numconfs = len(ensemble)
+    rmsd_distance_matrix = np.genfromtxt(file, delimiter=",")
+    numconfs = rmsd_distance_matrix.shape[0]
+    to_delete = geometry.get_duplicates_rmsd_matrix(rmsd_distance_matrix, threshold=args.threshold)
+    log.write(f"Number of conformers: {numconfs}\n")
+
+    ensemble = xyzutils.read_xyz_ensemble(f"{basename}.xyz")
     ens_elements = [_['elements'] for _ in ensemble.values()]
     ens_coordinates = [_['coordinates'] for _ in ensemble.values()]
     ens_header = [_['header'] for _ in ensemble.values()]
-    ens_mols = [rdkitutils.convert_coordinates_to_mols(ele, coords, removeHs=True) for ele, coords in zip(ens_elements, ens_coordinates)]
-
-    log.write(f"Current file: {basename}\n")
-    log.write(f"Number of conformers: {numconfs}\n")
-
-    start_time = time.time()
-
-    log.write("Using parallel RMSD calculator\n")
-    log.write(f"CPU Count: {os.cpu_count()}\n")
-
-    atomMap = rdkitutils.get_maximum_substructure_matches(ens_mols, max_matches=args.max_matches)
-    log.write(f"Number of atom maps to test: {len(atomMap)}\n")
-
-    rmsd_distance_matrix = rdkitutils.rmsd_matrix_parallel(ens_mols, atomMap)
-
-    log.write(f"Exporting .csv distance matrix...\n")
-    np.savetxt(f'{basename}.csv', rmsd_distance_matrix, delimiter=",")
-    log.write(f"Done\n")
-
-    to_delete = geometry.get_duplicates_rmsd_matrix(rmsd_distance_matrix, threshold=args.threshold)
     
-
     _coordinates = [ens_coordinates[i] for i in range(numconfs) if i not in to_delete]
     _elements = [ens_elements[i] for i in range(numconfs) if i not in to_delete]
     _header = [ens_header[i] for i in range(numconfs) if i not in to_delete]
@@ -81,5 +60,4 @@ end_time_global = time.time()
 elapsed_time_global = end_time_global - start_time_global
 log.write(f"Total wall clock time: {elapsed_time_global:.2f} seconds\n")
 
-log.close()
-        
+log.close()        
