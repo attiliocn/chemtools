41094983e4ff9d393182e60d5138cba311a942b2
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2025-02-05 14:11:12 -0300

    include downhill and endpoints to orca irc parser

--- chemtools/orca_parse-irc.py
+++ chemtools/orca_parse-irc.py
@@ -2,6 +2,7 @@
 
 import argparse 
 from modules import xyzutils
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('input', help='3-columns .csv input file. TS Geometry, TS Energy, FW Role. Must include header line')
@@ -31,13 +32,51 @@ for recipe in ts_recipes:
     )
 
     rv_trajectory = xyzutils.read_xyz_ensemble(f'{basename}_rv.trj')
+
+    if os.path.exists(f'downhill/{basename}_rv_down.trj'):
+        rv_downhill_trajectory = xyzutils.read_xyz_ensemble(f'downhill/{basename}_rv_down.trj')
+    else:
+        rv_downhill_trajectory = None
+
+    if os.path.exists(f'endpoints/{basename}_rv.xyz'):
+        rv_endpoint_geometry = xyzutils.read_xyz_ensemble(f'endpoints/{basename}_rv.xyz')
+        # rv_endpoint_geometry[0]['stringContent'] = xyzutils.build_xyz_file(
+        #     rv_endpoint_geometry[0]['elements'],
+        #     rv_endpoint_geometry[0]['coordinates'],
+        #     header=f'RV Endpoint REPLACE_ENERGY'
+        # )
+    else:
+        rv_endpoint_geometry = None
+
     fw_trajectory = xyzutils.read_xyz_ensemble(f'{basename}_fw.trj')
+
+    if os.path.exists(f'downhill/{basename}_fw_down.trj'):
+        fw_downhill_trajectory = xyzutils.read_xyz_ensemble(f'downhill/{basename}_fw_down.trj')
+    else:
+        fw_downhill_trajectory = None
+
+    if os.path.exists(f'endpoints/{basename}_rv.xyz'):
+        fw_endpoint_geometry = xyzutils.read_xyz_ensemble(f'endpoints/{basename}_fw.xyz')
+        # fw_endpoint_geometry[0]['stringContent'] = xyzutils.build_xyz_file(
+        #     fw_endpoint_geometry[0]['elements'],
+        #     fw_endpoint_geometry[0]['coordinates'],
+        #     header=f'FW Endpoint REPLACE_ENERGY'
+        # )
+    else:
+        fw_endpoint_geometry = None
 
     if recipe[2] == 0:
         # then fw is the trajectory toward reactants
         # fw trajectory must be reversed
         # the full path is fw(reversed) -> ts -> rv(unmodified)
         with open(f'{basename}.irc', mode='w') as f:
+            if fw_endpoint_geometry:
+                f.write(fw_endpoint_geometry[0]['stringContent'])
+
+            if fw_downhill_trajectory:
+                for mol_id in list(fw_downhill_trajectory.keys())[:0:-1]:
+                    f.write(fw_downhill_trajectory[mol_id]['stringContent'])
+
             for mol_id in list(fw_trajectory.keys())[::-1]:
                 f.write(fw_trajectory[mol_id]['stringContent'])
             
@@ -46,12 +85,26 @@ for recipe in ts_recipes:
             for mol_id in list(rv_trajectory.keys()):
                 f.write(rv_trajectory[mol_id]['stringContent'])
 
+            if rv_downhill_trajectory:
+                for mol_id in list(rv_downhill_trajectory.keys())[1:]:
+                    f.write(rv_downhill_trajectory[mol_id]['stringContent'])
+            
+            if rv_endpoint_geometry:
+                f.write(rv_endpoint_geometry[0]['stringContent'])
+
     elif recipe[2] == 1:
         # then fw is the trajectory toward products
         # fw trajectory remains unmodified
         # rv trajectory must be reversed
         # the full path is then rv(reversed) -> ts -> fw(unmodified)
         with open(f'{basename}.irc', mode='w') as f:
+            if rv_endpoint_geometry:
+                f.write(rv_endpoint_geometry[0]['stringContent'])
+
+            if rv_downhill_trajectory:
+                for mol_id in list(rv_downhill_trajectory.keys())[:0:-1]:
+                    f.write(rv_downhill_trajectory[mol_id]['stringContent'])
+
             for mol_id in list(rv_trajectory.keys())[::-1]:
                 f.write(rv_trajectory[mol_id]['stringContent'])
             
@@ -59,4 +112,11 @@ for recipe in ts_recipes:
 
             for mol_id in list(fw_trajectory.keys()):
                 f.write(fw_trajectory[mol_id]['stringContent'])
+
+            if fw_downhill_trajectory:
+                for mol_id in list(fw_downhill_trajectory.keys())[1:]:
+                    f.write(fw_downhill_trajectory[mol_id]['stringContent'])
+
+            if fw_endpoint_geometry:
+                f.write(fw_endpoint_geometry[0]['stringContent'])
             
